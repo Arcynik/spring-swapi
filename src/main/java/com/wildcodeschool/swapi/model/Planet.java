6cddfd37e727e52c060f7291c2d89609432bc755
Lea F <this.is.my.bot@gmail.com>
2019-10-29 09:31:38 +0100

    Y a tout qui marche :D

--- src/main/java/com/wildcodeschool/swapi/model/Planet.java
+++ src/main/java/com/wildcodeschool/swapi/model/Planet.java
@@ -2,12 +2,140 @@ package com.wildcodeschool.swapi.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.io.StringBufferInputStream;
+
 public class Planet {
 
-    // TODO : add attributes
+    private String name;
+    @JsonProperty("rotation_period")
+    private int rotationPeriod;
+    @JsonProperty("orbital_period")
+    private int orbitalPeriod;
+    private Long diameter;
+    private String climate;
+    private String gravity;
+    private String terrain;
+    @JsonProperty("surface_water")
+    private int surfaceWater;
+    private Long population;
+    private String[] residents;
+    private String[] films;
+    private String created;
+    private String edited;
+    private String url;
 
-    // TODO : add an empty constructor
+    public Planet() {
+    }
 
-    // TODO : add getters and setters
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRotationPeriod() {
+        return rotationPeriod;
+    }
+
+    public void setRotationPeriod(int rotationPeriod) {
+        this.rotationPeriod = rotationPeriod;
+    }
+
+    public int getOrbitalPeriod() {
+        return orbitalPeriod;
+    }
+
+    public void setOrbitalPeriod(int orbitalPeriod) {
+        this.orbitalPeriod = orbitalPeriod;
+    }
+
+    public Long getDiameter() {
+        return diameter;
+    }
+
+    public void setDiameter(Long diameter) {
+        this.diameter = diameter;
+    }
+
+    public String getClimate() {
+        return climate;
+    }
+
+    public void setClimate(String climate) {
+        this.climate = climate;
+    }
+
+    public String getGravity() {
+        return gravity;
+    }
+
+    public void setGravity(String gravity) {
+        this.gravity = gravity;
+    }
+
+    public String getTerrain() {
+        return terrain;
+    }
+
+    public void setTerrain(String terrain) {
+        this.terrain = terrain;
+    }
+
+    public int getSurfaceWater() {
+        return surfaceWater;
+    }
+
+    public void setSurfaceWater(int surfaceWater) {
+        this.surfaceWater = surfaceWater;
+    }
+
+    public Long getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(Long population) {
+        this.population = population;
+    }
+
+    public String[] getResidents() {
+        return residents;
+    }
+
+    public void setResidents(String[] residents) {
+        this.residents = residents;
+    }
+
+    public String[] getFilms() {
+        return films;
+    }
+
+    public void setFilms(String[] films) {
+        this.films = films;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public String getEdited() {
+        return edited;
+    }
+
+    public void setEdited(String edited) {
+        this.edited = edited;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
